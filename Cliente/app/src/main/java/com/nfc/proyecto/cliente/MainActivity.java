106d0eae9a6ed0d008baa481dcc633bd052dbdbe
sony <proyectoepsl@gmail.com>
2017-07-09 22:44:47 +0200

    fINAL CON ERRORES

---
+++ Cliente/app/src/main/java/com/nfc/proyecto/cliente/MainActivity.java
@@ -0,0 +1,281 @@
+package com.nfc.proyecto.cliente;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import android.support.design.widget.Snackbar;
+
+import java.io.BufferedReader;
+import java.nio.Buffer;
+import java.nio.charset.StandardCharsets;
+import android.util.Base64;
+
+
+public class MainActivity extends AppCompatActivity {
+    ///TextView mensaje;
+    String imei;
+    String respStr;
+    String resultado;
+    private static final int MY_WRITE_EXTERNAL_STORAGE = 0;
+    private View mLayout;
+    JSONObject obj;
+    Button IdenSala;
+
+    @Override
+    @TargetApi(Build.VERSION_CODES.M)
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        mLayout = findViewById(R.id.linearLayoutMain);
+
+        IdenSala = (Button) findViewById(R.id.IdenSala);
+        final ImageView imgViewer = (ImageView) findViewById(R.id.imageView);
+        final TextView mensaje = (TextView) findViewById(R.id.Mensaje);
+
+        IdenSala.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Ver imei del dispositivo
+                verifyPermission();
+                //final String imei = mensaje.getText().toString();//btenerImei();
+
+
+                //Enviar datos por post
+                new Thread(new Runnable(){
+                    @Override
+                    public void run() {
+                        try {
+                            //Cifrar imei
+                            Crypt aesCrypt = new Crypt();
+                            String a = aesCrypt.encrypt_string(imei);
+                            // Your implementation
+                            HttpClient httpClient = new DefaultHttpClient();
+
+
+                            //Llamamos al rest con los datos cifrados
+                            // HttpPost post = new HttpPost("https://proyectoepsl.pythonanywhere.com/rest_sala");
+                            HttpPost post = new HttpPost("http://192.168.2.129:8000/rest_sala/");
+                            post.setHeader("Content-Type", "application/json");
+                            post.setHeader("charset", "utf-8");
+                            //Construimos el objeto cliente en formato JSON
+                            JSONObject dato = new JSONObject();
+
+                            dato.put("Hash", a);
+
+
+                            StringEntity entity = new StringEntity(dato.toString());
+                            post.setEntity(entity);
+
+                            //Realizo el envío
+                            HttpResponse resp = httpClient.execute(post);
+
+                            //respStr = EntityUtils.toString(resp, HTTP.UTF_8);
+
+                            respStr = EntityUtils.toString(resp.getEntity());
+
+                            String _response=EntityUtils.toString(entity); // content will be consume only once
+                            final JSONObject jObject=new JSONObject(_response);
+
+                            obj = new JSONObject(respStr);
+                            resultado = obj.get("result").toString();
+                            if (resultado.equals("200")) {
+                                MainActivity.this.runOnUiThread(new Runnable() {
+                                    public void run () {
+                                        //Do something on UiThread
+                                        try {
+                                            byte[] byteArray = "".getBytes();
+
+                                            //JSONObject obj = new JSONObject(respStr);
+                                            byteArray = obj.get("Plano").toString().getBytes(StandardCharsets.US_ASCII);;
+                                            byteArray =  Base64.decode(byteArray, Base64.DEFAULT);
+
+                                            Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+                                            imgViewer.setImageBitmap(bitmap);
+
+                                            mensaje.setText("Plano Emergencia Sala Nº " + obj.get("Dependencia").toString());
+
+                                        } catch (Throwable t) {
+                                            Log.e("My App", "Could not parse malformed JSON: \"" + respStr + "\"");
+                                        }
+                                    }
+                                });
+
+                            } else{
+                                MainActivity.this.runOnUiThread(new Runnable() {
+                                    public void run () {
+                                        //Do something on UiThread
+                                        try {
+                                            imgViewer.setImageDrawable(getResources().getDrawable(R.drawable.error));
+                                            mensaje.setText(obj.get("Error").toString());
+                                        } catch (Throwable t) {
+                                            t.printStackTrace();
+                                        }
+                                    }
+                                });
+                            }
+
+                            //Gson gson = new Gson();
+                            // 2. JSON to Java object, read it from a Json String.
+                            //gson.fromJson(respStr, Sala.class);
+
+                            //mensaje.setText(respStr);
+                            //String plano = respStr[]
+                        }
+                        catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                }).start();
+
+            }
+        });
+
+        final Button LeerNfc = (Button) findViewById(R.id.nfc);
+        LeerNfc.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                try{
+                    Intent i = new Intent(view.getContext(), NfcActivity.class);
+                    //Obtenemos el numero de sala para cifrarlo pasandolo a la segunda actividad
+                    //i.putExtra("Sala", respStr.toString());
+                    i.putExtra("Sala_id", obj.get("IdSala").toString());
+                    //i.putExtra("Dependencia", obj.get("Dependencia").toString());
+                    //i.putExtra("Plano", obj.get("Plano").toString());
+                    startActivity(i);
+                }
+                catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Guarda el comentario
+     */
+    private void obtenerIMEI() {
+        TelephonyManager tel;
+        tel = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        //TextView mensaje = (TextView) findViewById(R.id.Imei);
+        imei= tel.getDeviceId().toString();
+        //mensaje.setText(imei);
+        //return imei;
+    }
+
+    //Paso 1. Verificar permiso
+    @TargetApi(Build.VERSION_CODES.M)
+    private void verifyPermission() {
+
+        //WRITE_EXTERNAL_STORAGE tiene implícito READ_EXTERNAL_STORAGE porque pertenecen al mismo
+        //grupo de permisos
+        if(Build.VERSION.SDK_INT>=23) {
+            int writePermission = checkSelfPermission(Manifest.permission.READ_PHONE_STATE);
+
+            if (writePermission != PackageManager.PERMISSION_GRANTED) {
+                requestPermission();
+            } else {
+                obtenerIMEI();
+            }
+        } else {
+            //simply use the required feature
+            //as the user has already granted permission to them during installation
+            obtenerIMEI();
+        }
+    }
+
+
+    //Paso 2: Solicitar permiso
+    @TargetApi(Build.VERSION_CODES.M)
+    private void requestPermission() {
+        //shouldShowRequestPermissionRationale es verdadero solamente si ya se había mostrado
+        //anteriormente el dialogo de permisos y el usuario lo negó
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_PHONE_STATE)) {
+            showSnackBar();
+        } else {
+            //si es la primera vez se solicita el permiso directamente
+            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE},
+                    MY_WRITE_EXTERNAL_STORAGE);
+        }
+    }
+
+    //Paso 3: Procesar respuesta de usuario
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        //Si el requestCode corresponde al que usamos para solicitar el permiso y
+        //la respuesta del usuario fue positiva
+        if (requestCode == MY_WRITE_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                obtenerIMEI();
+            } else {
+                showSnackBar();
+            }
+        }
+    }
+
+    /**
+     * Método para mostrar el snackbar de la aplicación.
+     * Snackbar es un componente de la librería de diseño 'com.android.support:design:23.1.0'
+     * y puede ser personalizado para realizar una acción, como por ejemplo abrir la actividad de
+     * configuración de nuestra aplicación.
+     */
+    private void showSnackBar() {
+        Snackbar.make(mLayout, R.string.permission_write_storage,
+                Snackbar.LENGTH_LONG)
+                .setAction(R.string.settings, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        openSettings();
+                    }
+                })
+                .show();
+    }
+
+    /**
+     * Abre el intento de detalles de configuración de nuestra aplicación
+     */
+    public void openSettings() {
+        Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        intent.setData(Uri.parse("package:" + getPackageName()));
+        startActivity(intent);
+    }
+
+    /* Checks if external storage is available for read and write */
+
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+}
---
+++ Cliente/app/src/main/java/com/nfc/proyecto/cliente/NfcActivity.java
@@ -0,0 +1,363 @@
+package com.nfc.proyecto.cliente;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import android.content.IntentFilter.MalformedMimeTypeException;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
+import android.os.AsyncTask;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import static android.nfc.NdefRecord.createMime;
+
+
+public class NfcActivity extends AppCompatActivity {
+    public static final String MIME_TEXT_PLAIN = "text/plain";
+    public static final String TAG = "NfcDemo";
+    private static String APP_TAG = "MyNFCBasic";
+
+    TextView mensaje = null;
+    private NfcAdapter mNfcAdapter;
+    String datosNfc = null;
+    String Sala_id;
+    String Dependencia;
+    String Plano;
+    ImageView imgViewer;
+    String resultado;
+    JSONObject obj;
+
+    /** Called when the activity is first created. */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.v(APP_TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc);
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        mensaje = (TextView) findViewById(R.id.nfc);
+        imgViewer = (ImageView) findViewById(R.id.imageView);
+
+        //Bundle datos = this.getIntent().getExtras();
+
+        Sala_id = getIntent().getStringExtra("Sala_id");
+        Dependencia = "";//datos.getString("Dependencia");
+        Plano = "";//datos.getString("Plano");
+
+
+
+
+        if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            //finish();
+            return;
+
+        }
+
+        if (!mNfcAdapter.isEnabled()) {
+            mensaje.setText("NFC is disabled.");
+        } else {
+            mensaje.setText("esto no se para que es");
+        }
+
+        handleIntent(getIntent());
+
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public void mostrarPlano(){
+        NfcActivity.this.runOnUiThread(new Runnable() {
+            public void run () {
+                //Do something on UiThread
+                try {
+                    byte[] byteArray = "".getBytes();
+
+                    //JSONObject obj = new JSONObject(respStr);
+                    byteArray = Plano.getBytes(StandardCharsets.US_ASCII);;
+                    byteArray =  Base64.decode(byteArray, Base64.DEFAULT);
+
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+                    imgViewer.setImageBitmap(bitmap);
+
+                    mensaje.setText("Plano Emergencia Sala Nº " + Dependencia);
+
+                } catch (Throwable t) {
+                    Log.e("My App", "Could not parse malformed JSON: \"");
+                }
+            }
+        });
+    }
+
+    public void cerrar(View view) {
+        finish();
+    }
+
+    public void onResume() {
+        super.onResume();
+        /**
+         * It's important, that the activity is in the foreground (resumed). Otherwise
+         * an IllegalStateException is thrown.
+         */
+        setupForegroundDispatch(this, mNfcAdapter);
+
+    }
+
+    @Override
+    protected void onPause() {
+        /**
+         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
+         */
+        stopForegroundDispatch(this, mNfcAdapter);
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        /**
+         * This method gets called, when a new Intent gets associated with the current activity instance.
+         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
+         * at the documentation.
+         *
+         * In our case this method gets called, when the user attaches a Tag to the device.
+         */
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        String action = intent.getAction();
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+
+            String type = intent.getType();
+            if (MIME_TEXT_PLAIN.equals(type)) {
+
+                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+                new NdefReaderTask().execute(tag);
+
+            } else {
+                Log.d(TAG, "Wrong mime type: " + type);
+            }
+        } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
+
+            // In case we would still use the Tech Discovered Intent
+            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            String[] techList = tag.getTechList();
+            String searchedTech = Ndef.class.getName();
+
+            for (String tech : techList) {
+                if (searchedTech.equals(tech)) {
+                    new NdefReaderTask().execute(tag);
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * @param activity The corresponding {@link Activity} requesting the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+
+        IntentFilter[] filters = new IntentFilter[1];
+        String[][] techList = new String[][]{};
+
+        // Notice that this is the same filter as in our manifest.
+        filters[0] = new IntentFilter();
+        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            filters[0].addDataType(MIME_TEXT_PLAIN);
+        } catch (MalformedMimeTypeException e) {
+            throw new RuntimeException("Check your mime type.");
+        }
+
+        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
+    }
+
+    /**
+     * @param activity The corresponding {@link //BaseActivity} requesting to stop the foreground dispatch.
+     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
+     */
+    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
+        adapter.disableForegroundDispatch(activity);
+    }
+
+    /**
+     * Background task for reading the data. Do not block the UI thread while reading.
+     *
+     * @author Ralf Wondratschek
+     *
+     */
+    private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
+
+        @Override
+        protected String doInBackground(Tag... params) {
+            Tag tag = params[0];
+
+            Ndef ndef = Ndef.get(tag);
+            if (ndef == null) {
+                // NDEF is not supported by this Tag.
+                return null;
+            }
+
+            NdefMessage ndefMessage = ndef.getCachedNdefMessage();
+
+            NdefRecord[] records = ndefMessage.getRecords();
+            for (NdefRecord ndefRecord : records) {
+                if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
+                    try {
+                        //obtengo los datos del nfc
+                        return readText(ndefRecord);
+                    } catch (UnsupportedEncodingException e) {
+                        Log.e(TAG, "Unsupported Encoding", e);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private String readText(NdefRecord record) throws UnsupportedEncodingException {
+        /*
+         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
+         *
+         * http://www.nfc-forum.org/specs/
+         *
+         * bit_7 defines encoding
+         * bit_6 reserved for future use, must be 0
+         * bit_5..0 length of IANA language code
+         */
+
+            byte[] payload = record.getPayload();
+
+            // Get the Text Encoding
+            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
+
+            // Get the Language Code
+            int languageCodeLength = payload[0] & 0063;
+
+            // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
+            // e.g. "en"
+
+            // Get the Text
+            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (result != null) {
+                mensaje.setText("Read content: " + result);
+                datosNfc=result;
+                //result=result.replace("\n","").split("\r");
+                //ciframos el dni
+                new Thread(new Runnable(){
+                    @Override
+                    public void run() {
+                        try {
+                            //Cifrar imei
+                            Crypt aesCrypt = new Crypt();
+                            String a = aesCrypt.encrypt_string(Sala_id);
+                            String b = aesCrypt.encrypt_string(datosNfc.replace("\n","").split("\r")[2]);
+                            // Your implementation
+                            HttpClient httpClient = new DefaultHttpClient();
+
+
+                            //Llamamos al rest con los datos cifrados
+                            // HttpPost post = new HttpPost("https://proyectoepsl.pythonanywhere.com/rest_sala");
+                            HttpPost post = new HttpPost("http://192.168.2.129:8000/rest_usuario/");
+                            post.setHeader("Content-Type","application/json");
+                            post.setHeader("charset","utf-8");
+                            //Construimos el objeto cliente en formato JSON
+                            JSONObject dato = new JSONObject();
+
+
+                            dato.put("Sala_id", a);
+                            dato.put("Dni",b);
+
+                            Log.i("JSON", dato.toString());
+
+                            StringEntity entity = new StringEntity(dato.toString());
+                            post.setEntity(entity);
+
+                            //Realizo el envío
+                            HttpResponse resp = httpClient.execute(post);
+
+                            String respStr = EntityUtils.toString(resp.getEntity());
+                            obj = new JSONObject(respStr);
+                            resultado = obj.get("result").toString();
+                            if (resultado.equals("200")) {
+                                NfcActivity.this.runOnUiThread(new Runnable() {
+                                    public void run () {
+                                        //Do something on UiThread
+                                        try {
+                                            imgViewer.setImageDrawable(getResources().getDrawable(R.drawable.ok));
+                                            mensaje.setText("Bienvenido");
+                                        } catch (Throwable t) {
+                                            t.printStackTrace();
+                                        }
+                                    }
+                                });
+                            }
+                            else{
+                                NfcActivity.this.runOnUiThread(new Runnable() {
+                                    public void run () {
+                                        //Do something on UiThread
+                                        try {
+                                            imgViewer.setImageDrawable(getResources().getDrawable(R.drawable.error));
+                                            mensaje.setText(obj.get("Error").toString());
+
+                                        } catch (Throwable t) {
+                                            t.printStackTrace();
+                                        }
+                                    }
+                                });
+
+                            }
+                            Thread.sleep (5000);
+                            finish();
+                            return;
+                        }
+                        catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        }
+    }
+}
